d9ed0555e0921131f4e215eb38e1844f7a346be3
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-22 17:19:58 +0700

    memanggil fungsi MahasiswaApp

--- app/src/main/java/com/example/navigasidengandata/MainActivity.kt
+++ app/src/main/java/com/example/navigasidengandata/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigasidengandata.navigasi.Halaman
+import com.example.navigasidengandata.navigasi.MahasiswaApp
 import com.example.navigasidengandata.ui.theme.NavigasidenganDataTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigasidenganDataTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MahasiswaApp(modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
