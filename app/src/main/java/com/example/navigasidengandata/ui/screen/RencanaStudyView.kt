36f1dd4dd77d8a884f64f83660966cd66c537221
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-11-22 16:19:55 +0700

    menambahkan variable pada fungsi RencanaStudyView

--- app/src/main/java/com/example/navigasidengandata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasidengandata/ui/screen/RencanaStudyView.kt
@@ -52,4 +52,15 @@ fun RencanaStudyView(
     mahasiswa: Mahasiswa,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
-) {}
+) {
+    var chosenDropdown by remember {
+        mutableStateOf(
+            " "
+        )
+    }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember {
+        mutableStateOf(" ")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+}
